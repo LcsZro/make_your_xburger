3e7dccf4069d3e954dd46d4279d71165cd367cbf
Lucas <86057306+LcsZro@users.noreply.github.com>
2022-09-20 19:50:20 -0300

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="footer">
+        <p>Make Your Burger &copy; 2022</p>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'Footer'
+}
+</script>
+
+<style>
+ #footer{
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #222;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+ }
+</style>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="main-container">
+    <h1>Pedido</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
