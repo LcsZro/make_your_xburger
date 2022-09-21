2f01b83beaa7c58c8e2849626f91b5f475aa9f9f
Lucas <86057306+LcsZro@users.noreply.github.com>
2022-09-21 19:01:51 -0300

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
 <template class="main-container">
+  <Banner />
   <div>
-    <h1>Home</h1>
+    <h1>Monte o seu Burger:</h1>
+    <p>formulário</p>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue";
 
 export default {
-
+  name: "Home",
+  components: {
+    Banner,
+    Banner
+}
 }
 
 </script>
