f7de3e004601b2be2e29572f76d0af61de29a1fd
Lucas <86057306+LcsZro@users.noreply.github.com>
2022-09-22 14:43:33 -0300

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,20 @@
   <div>
     <h1>Monte o seu Burger:</h1>
     <p>formulário</p>
+    <Burgerform />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import Burgerform from "../components/Burgerform.vue";
 
 export default {
   name: "Home",
   components: {
     Banner,
-    Banner
-}
+    Burgerform,
+  } 
 }
 
 </script>
