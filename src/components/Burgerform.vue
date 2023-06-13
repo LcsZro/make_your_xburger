31f913d70ca385e2d687ff934ea9d6e53b7dfa89
Lucas <86057306+LcsZro@users.noreply.github.com>
2023-06-13 13:57:35 -0300

    up

--- src/components/Burgerform.vue
+++ src/components/Burgerform.vue
@@ -1,38 +1,37 @@
 <template>
-  <div>
-    <div v-if="msg">
-      <div class="message-container">
-        <Message :msg="msgForm" />
+  <div id="form">
+      <div >
+          <Message v-bind:msg = "msgForm" v-if="msg"/>
       </div>
-    </div>
-    <form id="burger-form" @submit="createBurger">
-      <div class="input-container">
-        <label for="nome">Nome</label>
-        <input id="nome" name="name" v-model="nome" placeholder="Digite seu nome" type="text">
-      </div>
-      <div class="input-container">
-        <label for="pao">Escolha o pão:</label>
-        <select name="pao" id="pao" v-model="pao">
-          <option v-bind:value="pao.tipo" v-for="pao in paes" v-bind:key="pao.id">{{ pao.tipo }}</option>
-        </select>
-      </div>
-      <div class="input-container">
-        <label for="carne">Escolha sua carne:</label>
-        <select name="carne" id="carne" v-model="carne">
-          <option v-bind:value="carne.tipo" v-for="carne in carnes" v-bind:key="carne.id">{{ carne.tipo }}</option>
-        </select>
-      </div>
-      <div class="input-container">
-        <label for="pao">Selecione os opcionais</label>
-        <div v-for="opcional in opcionaisdata" v-bind:key="opcional.tipo" class="checkbox-container">
-          <input type="checkbox" name="opcionais" id="" v-model="opcionais" v-bind:value="opcional.tipo">
-          <span>{{ opcional.tipo }}</span>
-        </div>
-      </div>
-      <div class="input-container">
-        <input type="submit" value="Criar meu Burger!" class="submit-btn">
-      </div>
-    </form>
+      <form action="" id="burger-form" @submit="createBurger">
+          <div class="input-container">
+              <label for="nome">Nome</label>
+              <input id="nome" name="name" v-model="nome" placeholder="Digite seu nome" type="text">
+          </div>
+          <div class="input-container">
+              <label for="pao">Escolha o pao: </label>
+              <select name="pao" id="pao" v-model="pao">
+                  <option v-bind:value="pao.tipo" v-for="pao in paes" v-bind:key="pao.id"> {{ pao.tipo }} </option>
+              </select>
+          </div>
+          <div class="input-container">
+              <label for="carne">Escolha sua carne: </label>
+              <select name="carne" id="carne" v-model="carne">
+                  <option v-bind:value="carne.tipo" v-for="carne in carnes" v-bind:key="carne.id"> {{ carne.tipo }} </option>
+              </select>
+          </div>
+
+          <div class="input-container">
+              <label for="pao" id="op-title">Seleciona os opcionais</label>
+              <div class="checkbox-container" v-for="opcional in opcionaisdata" v-bind:key="opcional.tipo" >
+                  <input type="checkbox" name="opcionais" id="" v-model="opcionais" v-bind:value="opcional.tipo">
+                  <span>{{ opcional.tipo }} </span>
+              </div>
+          </div>
+          <div class="input-container">
+              <input type="submit" class="submit-btn"  value="Enviar">
+          </div>
+      </form>
   </div>
 </template>
 
@@ -40,71 +39,71 @@
 import Message from './Message.vue'
 
 export default {
-  name: "BurgerForm",
-  components: {
-    Message
-  },
-  data() {
-    return {
-      msgForm: "",
-      msg: false,
-      pedidoSet: false,
-      carnes: null,
-      paes: null,
-      opcionaisdata: null,
-      nome: null,
-      carne: null,
-      pao: null,
-      opcionais: [],
-      status: "Solicitado"
-    }
-  },
-  methods: {
-    async getIngrdientes() {
-      const req = await fetch("https://json-backend.vercel.app/ingredientes")
-      const data = await req.json()
-
-      this.carnes = data.carnes
-      this.paes = data.paes
-      this.opcionaisdata = data.opcionais
+    name: "BurgerForm",
+    components: {
+        Message
     },
+    data() {
+        return {
+            msgForm: "",
+            msg: false,
+            pedidoSet: false,
+            carnes: null,
+            paes: null,
+            opcionaisdata: null,
+            nome: null,
+            carne: null,
+            pao: null,
+            opcionais: [],
+            status: "Solicitado"
+            }
+        },
+    methods: {
+        async getIngrdientes() {
+            const req = await fetch("https://json-backend.vercel.app/ingredientes")
+            const data = await req.json()
+            
+            this.carnes = data.carnes
+            this.paes = data.paes
+            this.opcionaisdata  = data.opcionais
+        },
 
-    async createBurger(e) {
-      e.preventDefault()
-      const data = {
-        nome: this.nome,
-        carne: this.carne,
-        pao: this.pao,
-        opcionais: this.opcionais,
-        status: "Solicitado"
-      }
+        async createBurger(e) {
+            e.preventDefault()
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: this.opcionais,
+                status: "Solicitado"
+            }
 
-      const req = await fetch("https://json-backend.vercel.app/burgers", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-      })
+            const req = await fetch("https://json-backend.vercel.app/burgers", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(data)
+            }) 
 
-      const resposta = await req.json()
+            const resposta = await req.json()
+            
+            // Mostrando mensagem
+            this.msgForm = `Pedido N ${resposta.id} criado com sucesso!`
+            this.msg = true
 
-      // Mostrando mensagem
-      this.msgForm = `Pedido Nº ${resposta.id} criado com sucesso!`
-      this.msg = true
+            // Limpando campos 
 
-      // Limpando campos
-      this.nome = ""
-      this.carne = ""
-      this.pao = ""
-      this.opcionais = []
+            this.nome = ""
+            this.carne = ""
+            this.pao = ""
+            this.opcionais = []
 
-      setTimeout(() => {
-        this.msg = false
-      }, 3000)
+            setTimeout(() => this.msg = false, 3000)
+            
+        }
+    },
+    mounted() {
+        this.getIngrdientes()
     }
-  },
-  mounted() {
-    this.getIngrdientes()
-  }
 }
 </script>
 
@@ -167,7 +166,7 @@ select {
   transition: .5s;
 }
 
-.submit-btn:hover {
+:hover {
   background: transparent;
   color: #222;
 }
